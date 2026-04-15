ce530c138c248f391cee5689a82f24e4abb0514c
MotusSkarnes <resepsjon@motus-skarnes.no>
2026-04-15 15:16:55 +0200

    Create supabase.ts

---
+++ src/lib/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://upjujlfhwykbgjqlaxjf.supabase.co",
+  "sb_publishable_zexVM2CTt_1DJ7h_zeLllg_0KvCRZoJ"
+);
